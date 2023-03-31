7d31a3f6ed017eacf04491ba2495fa849d7abf18
abraham reta <95265657+areta1921@users.noreply.github.com>
2023-03-30 23:54:08 -0400

    fix bugs

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,7 +4,7 @@ import { BsPatchCheckFill } from "react-icons/bs";
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>Whar Skills I Have</h5>
+      <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
       <div className="container experience_container">
         <div className="experience__frontend">
